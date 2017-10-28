24c829d8a1480af30d2bb7507b069085c7304f98
theotherjonjanes <theotherjonjanes@users.noreply.github.com>
2017-10-28 15:02:21 -0400

    Naming

--- Green-Machine/green-machine.py
+++ Green-Machine/green-machine.py
@@ -1,9 +1,8 @@
 import datetime
 
 now = datetime.datetime.now()
-
 dateAndTime = now.strftime("%d-%b-%Y %I:%M%p")
 
-f = open('green-machine.txt','a')
+f = open('green-machine.txt', 'a')
 f.write('\n' + dateAndTime)
 f.close()
